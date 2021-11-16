c76ca52c903467ec3599df5fed6effbcf53490a7
hanjin.kim <rragw112@naver.com>
2021-11-16 10:59:57 +0900

    implement add and subtract

--- git_practice/list_ops.py
+++ git_practice/list_ops.py
@@ -5,6 +5,14 @@ def add_lists(foo, bar):
 		dst.append(foo[i] + bar[i])
 	return dst
 
+def subtract(foo, bar):
+    out = []
+    for f, b in zip(foo, bar):
+        out.append(f - b)
+    return out
+
+
 if __name__ == "__main__":
 	dst = add_lists([1,2,3], [4,5,6])
 	print(dst)
+
